6500dbdcfcf80bad6f6a15455c75a1ac5dae85cc
Mariusz Kogut <mk@handmade-systems.de>
2020-03-07 08:18:51 +0100

    Make footer fixed

--- frontend/src/shared/components/footer.tsx
+++ frontend/src/shared/components/footer.tsx
@@ -1,9 +1,17 @@
 import React, { FunctionComponent, useMemo } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 const Footer: FunctionComponent = () => {
   const year = useMemo<number>(() => new Date().getFullYear(), []);
 
-  return <footer className="p-3">© {year}</footer>;
+  return (
+    <footer className="p-3">
+      <Navbar fixed="bottom" bg="light">
+        <Nav className="mr-auto">© {year}</Nav>
+      </Navbar>
+    </footer>
+  );
 };
 
 export default Footer;
